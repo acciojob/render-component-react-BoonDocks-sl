355bbc7afafe71b77d875cfb8a6284dba3f56956
Supriya Limbole <limbole.supriya21@gmail.com>
2023-07-15 12:58:55 +0530

    submitting code-Sat Jul 15 2023 12:58:15 GMT+0530 (India Standard Time)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-
+const App = () => {
+    return (
+        < div >
+            <p>I am learning React. My life is getting better.</p>
+        </div>
+    );
+};
 ReactDOM.render(<App />, document.getElementById("root"));
